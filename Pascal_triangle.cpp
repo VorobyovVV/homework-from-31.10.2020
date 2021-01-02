7d20f5f87951de89ec1dcc7fc1e3096a47db7a2e
Воробьёв Владислав <72572241+VorobyovVV@users.noreply.github.com>
2021-01-02 11:38:19 +0500

    Add files via upload

---
+++ Pascal_triangle.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+
+
+int main()
+{
+    int n;
+    std::cout << "Input n : ";
+    std::cin >> n;
+    std::cout << std::endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        int s = 1;
+        for (int j = 1; j < (n - i); j++)
+        {
+            std::cout << "\r";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            std::cout << " " << s;
+            s = s * (i - k) / (k + 1);
+        }
+        std::cout << std::endl ;
+    }
+    std::cout << std::endl;
+    return 0;
+}
